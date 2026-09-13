6fd0fb55c2ba52f3138f846cba2e823a3d5a12c1
sara alshahrani <saraalshahrani@saras-MacBook-Neo.local>
2026-09-13 11:44:52 +0300

    first task

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python3
+lang= "python"
+version = 3
+pi_val = 3.14
+is_valid= 10>2
+
+print(f"Language: , {lang}")
+print(f"Version: , {version}")
+print(f"Pi approx: , {pi_val}")
+print(f"Comptaion: , {is_valid}")
