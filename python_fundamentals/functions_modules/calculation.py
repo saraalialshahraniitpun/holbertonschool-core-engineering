42f0cd2dfd3cf66ea562afbbc2768922349857ff
sara alshahrani <saraalshahrani@saras-MacBook-Neo.local>
2026-09-19 14:09:01 +0300

    fixed

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module to perform calculations using calculator_1 module."""
+
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
---
+++ python_fundamentals/functions_modules/calculator_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Calculator 1 module with basic arithmetic functions."""
+
+
+def add(a, b):
+    """Return the sum of a and b."""
+    return a + b
+
+
+def sub(a, b):
+    """Return the difference of a and b."""
+    return a - b
+
+
+def mul(a, b):
+    """Return the product of a and b."""
+    return a * b
+
+
+def div(a, b):
+    """Return the division of a and b."""
+    return int(a / b)
