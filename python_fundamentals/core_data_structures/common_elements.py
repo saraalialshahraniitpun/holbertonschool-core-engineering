9e3ac59919647c62c4425b67176e3bb0196c3847
sara alshahrani <saraalshahrani@saras-MacBook-Neo.local>
2026-09-19 14:34:37 +0300

    Add common_elements function task

---
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that contains a function to find
+common elements in two sets.
+"""
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return set_1 & set_2
