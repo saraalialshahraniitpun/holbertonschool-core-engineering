1a6312089f6a6a332fe6c4b9fb991b4ad2a8114b
sara alshahrani <saraalshahrani@saras-MacBook-Neo.local>
2026-09-26 10:28:54 +0300

    Add safe_print_list function task

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,6 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module that contains a function to safely
+print x elements of a list.
+"""
+
 
 def safe_print_list(my_list=[], x=0):
+    """Print x elements of a list safely
+    and return the real number of elements printed.
+    """
     nb_print = 0
     for i in range(x):
         try:
