c6f8a9af092284af3b5b66462d548654cc697403
sara alshahrani <saraalshahrani@saras-MacBook-Neo.local>
2026-09-26 10:34:11 +0300

    Add safe_print_list_integers function task with proper exception handling

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -11,11 +11,10 @@ def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            nb_print += 1
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                nb_print += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print()
     return nb_print
